bd35ebe8f0bdcd24a8e413920e127bc6d6b768f9
IT_그룹_박은지 <oi9340@englishegg.co.kr>
2025-04-09 10:30:20 +0900

    [feat] Route 파일 분리

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,28 +1,10 @@
 <?php
 
-use CodeIgniter\Router\RouteCollection;
+// -------------------------------------------------------------
+// Load Route Files from app/Routes/*.php
+// -------------------------------------------------------------
+$routesPath = APPPATH . 'Routes';
 
-/**
- * @var RouteCollection $routes
- */
-$routes->get('/', 'Home::index');
-$routes->get('/swagger', 'Swagger::generate'); //public에 swagger.json 파일 생성
-$routes->get('/swagger-api', 'Swagger::index'); //public에 swagger.json 파일 생성
-
-$routes->get('/health', 'HealthCheck::index'); // health check
-
-$routes->get('/api/v1/retail_eggtv', 'Products\EggTv\Ticket::readAllTickets');
-$routes->post('/api/v1/retail_eggtv', 'Products\EggTv\Ticket::createTicket');
-$routes->get('/api/v1/retail_eggtv/(:num)', 'Products\EggTv\Ticket::readOneTicket/$1');
-$routes->delete('/api/v1/retail_eggtv/(:num)', 'Products\EggTv\Ticket::deleteTicket/$1');
-$routes->get('/api/v1/retail_eggtv/(:num)/issues', 'Products\EggTv\Ticket::readAllTicketIssues/$1');
-$routes->post('/api/v1/retail_eggtv/(:num)/issues', 'Products\EggTv\Ticket::createTicketIssue/$1');
-$routes->get('/api/v1/retail_eggtv/(:num)/issues/(:num)', 'Products\EggTv\Ticket::readOneTicketIssue/$1/$2');
-$routes->put('/api/v1/retail_eggtv/(:num)/issues/(:num)', 'Products\EggTv\Ticket::updateTicketIssue/$1/$2');
-$routes->get('/api/v1/retail_eggtv/(:num)/issues/(:num)/codes', 'Products\EggTv\Ticket::readAllTicketCodes/$1/$2');
-$routes->put('/api/v1/retail_eggtv/(:num)/issues/(:num)/codes', 'Products\EggTv\Ticket::updateTicketCode/$1/$2');
-$routes->get('/api/v1/retail_eggtv/search', 'Products\EggTv\Ticket::searchTicketCode');
-$routes->get('/api/v1/retail_eggtv/registered', 'Products\EggTv\Ticket::registeredTicket');
-$routes->delete('/api/v1/retail_eggtv/registered', 'Products\EggTv\Ticket::retrieveRegisteredTicket');
-$routes->get('/api/v1/retail_eggtv/used', 'Products\EggTv\Ticket::usedTicket');
-$routes->delete('/api/v1/retail_eggtv/used', 'Products\EggTv\Ticket::retrieveUsedTicket');
+foreach (glob("$routesPath/*.php") as $routeFile) {
+    require $routeFile;
+}
---
+++ app/Routes/retail_products.php
@@ -0,0 +1,22 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/api/v1/retail_eggtv', 'Products\EggTv\Ticket::readAllTickets');
+$routes->post('/api/v1/retail_eggtv', 'Products\EggTv\Ticket::createTicket');
+$routes->get('/api/v1/retail_eggtv/(:num)', 'Products\EggTv\Ticket::readOneTicket/$1');
+$routes->delete('/api/v1/retail_eggtv/(:num)', 'Products\EggTv\Ticket::deleteTicket/$1');
+$routes->get('/api/v1/retail_eggtv/(:num)/issues', 'Products\EggTv\Ticket::readAllTicketIssues/$1');
+$routes->post('/api/v1/retail_eggtv/(:num)/issues', 'Products\EggTv\Ticket::createTicketIssue/$1');
+$routes->get('/api/v1/retail_eggtv/(:num)/issues/(:num)', 'Products\EggTv\Ticket::readOneTicketIssue/$1/$2');
+$routes->put('/api/v1/retail_eggtv/(:num)/issues/(:num)', 'Products\EggTv\Ticket::updateTicketIssue/$1/$2');
+$routes->get('/api/v1/retail_eggtv/(:num)/issues/(:num)/codes', 'Products\EggTv\Ticket::readAllTicketCodes/$1/$2');
+$routes->put('/api/v1/retail_eggtv/(:num)/issues/(:num)/codes', 'Products\EggTv\Ticket::updateTicketCode/$1/$2');
+$routes->get('/api/v1/retail_eggtv/search', 'Products\EggTv\Ticket::searchTicketCode');
+$routes->get('/api/v1/retail_eggtv/registered', 'Products\EggTv\Ticket::registeredTicket');
+$routes->delete('/api/v1/retail_eggtv/registered', 'Products\EggTv\Ticket::retrieveRegisteredTicket');
+$routes->get('/api/v1/retail_eggtv/used', 'Products\EggTv\Ticket::usedTicket');
+$routes->delete('/api/v1/retail_eggtv/used', 'Products\EggTv\Ticket::retrieveUsedTicket');
